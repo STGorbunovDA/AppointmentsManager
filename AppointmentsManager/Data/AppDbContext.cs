cd5b0c410778a7c07e926ce49c4733d00f6c6855
STGorbunovDA <bous07@mail.ru>
2023-10-12 01:28:14 +0800

    add AppDbContext

---
+++ AppointmentsManager/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using AppointmentsManager.Data.Models;
+using System.Collections.Generic;
+
+namespace AppointmentsManager.Data
+{
+    //Add-Migration -Name Initial -OutputDir "Data/Migrations"
+    //Update-Database
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Appointment> Appointments { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
